58d9e80573d82372c89d7ea0f0ad6569f2db940a
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-06 22:28:54 +0700

    Tipe Data Primitif

---
+++ test/tipe-date.test.ts
@@ -0,0 +1,11 @@
+describe("Data type", () => {
+  it("should must declare", () => {
+    let name: string = "Rizal Pradana";
+    let balence: number = 2000;
+    let isVip: boolean = true;
+
+    console.info(name);
+    console.info(balence);
+    console.info(isVip);
+  });
+});
