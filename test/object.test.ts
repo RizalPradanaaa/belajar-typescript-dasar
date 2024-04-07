ae6f1a722a4ff0cda0aabe9a276f6a2f44f5b34e
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-07 21:00:08 +0700

    Optional Properties

--- test/object.test.ts
+++ test/object.test.ts
@@ -1,12 +1,13 @@
 describe("Object Type", () => {
   it("should support in typescript", () => {
-    const person: { id: number; name: string } = {
+    const person: { id: number; name: string; hoobies?: string[] } = {
       id: 1,
       name: "Rizal",
     };
     console.info(person);
     person.id = 2;
     person.name = "Pradana";
+    person.hoobies = ["Coding"];
     console.info(person);
   });
 });
