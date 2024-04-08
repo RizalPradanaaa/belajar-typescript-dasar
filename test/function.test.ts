202443e43295409e6685bf794ebbd255c7b7fcca
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-08 15:53:05 +0700

    Function Paramater

--- test/function.test.ts
+++ test/function.test.ts
@@ -10,4 +10,23 @@ describe("Function in typescript", () => {
     }
     printName("Boy");
   });
+
+  it("should support default value", () => {
+    function sayHello(name: string = "Guest"): string {
+      return `Hello ${name}`;
+    }
+    expect(sayHello("Rizal")).toBe("Hello Rizal");
+    expect(sayHello()).toBe("Hello Guest");
+  });
+
+  it("should support rest paramater", () => {
+    function sum(...values: number[]): number {
+      let total = 0;
+      for (const value of values) {
+        total += value;
+      }
+      return total;
+    }
+    expect(sum(1, 2, 3, 4, 5)).toBe(15);
+  });
 });
